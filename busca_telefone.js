c1b4bb25d64d4c53f5bb82c5a1993562fdbf4385
Vitor Candido <vitor.candido900@gmail.com>
2025-06-25 14:21:24 -0300

    Incluindo busca celular

--- busca_telefone.js
+++ busca_telefone.js
@@ -6,3 +6,8 @@ const regexTelefone = /\(\d+\)\s\d+-\d+/g
 
 const matchTelefone = banco.match(regexTelefone)
 console.log(matchTelefone);
+
+const patternCel = /\(\d{2}\)\s\d{5}-\d{4}/g
+
+const matchCelular = banco.match(patternCel)
+console.console(matchCelular);
